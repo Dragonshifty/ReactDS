29806e2ab7ffeee7aa72f68231b4272a1fa5dd75
James Fitt <91226933+Dragonshifty@users.noreply.github.com>
2024-10-23 17:22:12 +0100

    CSS Diffrentia Logo

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     return (
         <div className="element">
             <Navbar />
-            <BodyLayout />
+            {/* <BodyLayout /> */}
             <DiffrentiaBody />
         </div>
     );
--- src/Diffrentia/Components/DiffrentiaBody.tsx
+++ src/Diffrentia/Components/DiffrentiaBody.tsx
@@ -10,18 +10,31 @@ export const DiffrentiaBody = () => {
                     backgroundPosition: "center",
                     backgroundSize: "cover",
                     backgroundRepeat: "no-repeat",
+                    paddingTop: "2rem",
+                    minHeight: "100vh",
                 }}
             >
-                <div className="container">
+                <div className="container   ">
                     <div className="d-flex justify-content-center mb-4 position-relative">
                         <img
-                            className="img-fluid"
-                            src="/images/diffrentia/quad.png"
+                            className="img-fluid rotating-image position-absolute"
+                            src="/images/diffrentia/quadouter.png"
                             height="300"
                             width="300"
                             alt=""
+                            style={{ zIndex: 1 }}
                         />
-                        <h1 className="cinzel-decorative-regular ">
+                        <img
+                            className="img-fluid"
+                            src="/images/diffrentia/quadinner.png"
+                            height="400"
+                            width="400"
+                            alt=""
+                        />
+                        <h1
+                            className="cinzel-decorative-regular "
+                            style={{ zIndex: 2 }}
+                        >
                             Diffrentia
                         </h1>
                     </div>
